60db755c305082322c2bef58687175397542d491
tyler.moore <tyler.moore@rajones.com>
2024-03-22 15:01:04 -0400

    Rev 1 completed and tested

--- RP2040_USB.ino
+++ RP2040_USB.ino
@@ -1,64 +1,81 @@
 #include <Arduino.h>
-#include <Keyboard.h>
 #include <Adafruit_NeoPixel.h>
+#include <Keyboard.h>
 
-#define LED_PIN 25 // The built-in LED pin on the XIAO RP2040
-#define NEOPIXEL_PIN 22 // NeoPixel pin
-#define NEOPIXEL_COUNT 1 // Number of NeoPixels
+const int BUTTON_GPIO = D0; // GPIO pin for the button input
+unsigned long lastButtonPressTime = 0;
+unsigned long lastLongHoldReportTime = 0;
+bool longHoldDetected = false;
+bool shortHoldDetected = false;
 
-#define DOWN_ARROW_GPIO 2 // GPIO pin for Down Arrow input
-#define UP_ARROW_GPIO 3 // GPIO pin for Up Arrow input
+int Power = 11;
+int PIN  = 12;
+#define NUMPIXELS 1
 
-Adafruit_NeoPixel pixels(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT); // Set the LED pin as an output
-  digitalWrite(LED_PIN, LOW); // Turn off the LED initially
-
-  pinMode(DOWN_ARROW_GPIO, INPUT_PULLUP); // Set Down Arrow input pin
-  pinMode(UP_ARROW_GPIO, INPUT_PULLUP); // Set Up Arrow input pin
-
+  Serial.begin(9600); // Initialize serial communication
+  pinMode(BUTTON_GPIO, INPUT_PULLDOWN); // Set button input pin with internal pull-down resistor
+  pinMode(Power, OUTPUT);
+  digitalWrite(Power, HIGH);
   pixels.begin(); // Initialize NeoPixel
-
-  // Initialize USB Keyboard
-  Keyboard.begin();
-  delay(2000); // delay 2 seconds for setup
+  pixels.setPixelColor(0, pixels.Color(0, 0, 0)); // Turn off NeoPixel initially
+  pixels.show();
+  Keyboard.begin(); // Initialize USB Keyboard
 }
 
 void loop() {
-  static bool downArrowPressed = false;
-  static bool upArrowPressed = false;
+  if (digitalRead(BUTTON_GPIO) == HIGH && millis() - lastButtonPressTime > 100) { // Button is pressed and debounce timer (100ms) has elapsed
+    unsigned long buttonPressStartTime = millis();
 
-  // Check for rising edge of Down Arrow input
-  if (digitalRead(DOWN_ARROW_GPIO) == LOW && !downArrowPressed) {
-    Keyboard.press(KEY_DOWN_ARROW); // Press Down Arrow key
-    delay(100); // Optional delay for key press
-    Keyboard.release(KEY_DOWN_ARROW); // Release Down Arrow key
-    pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // Set NeoPixel to Red
-    pixels.show();
-    downArrowPressed = true;
+    // Wait for button release or long hold
+    while (digitalRead(BUTTON_GPIO) == HIGH) {
+      if (millis() - buttonPressStartTime > 1500) { // Long hold (> 1500ms)
+        if (!longHoldDetected) {
+          Serial.println("Long hold");
+          pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // Turn NeoPixel red
+          pixels.show();
+          Keyboard.press(KEY_UP_ARROW);
+          delay(50);
+          Keyboard.release(KEY_UP_ARROW);
+          longHoldDetected = true;
+          lastLongHoldReportTime = millis();
+          break;
+        }
+        else if (millis() - lastLongHoldReportTime >= 1000) {
+          Serial.println("Long hold continued");
+          Keyboard.press(KEY_UP_ARROW);
+          delay(50);
+          Keyboard.release(KEY_UP_ARROW);
+          lastLongHoldReportTime = millis();
+        }
+      }
+    }
+
+    if (!longHoldDetected && digitalRead(BUTTON_GPIO) == LOW) { // Short hold (< 2500ms)
+      delay(100); // Wait for debounce
+      if (digitalRead(BUTTON_GPIO) == LOW && !shortHoldDetected) { // Button is released
+        Serial.println("Short hold");
+        pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // Turn NeoPixel green
+        pixels.show();
+        Keyboard.press(KEY_DOWN_ARROW); // Send down arrow through keyboard
+        delay(50);
+        Keyboard.release(KEY_DOWN_ARROW);
+        delay(1000); // Wait for 1 second
+        pixels.setPixelColor(0, pixels.Color(0, 0, 0)); // Turn off NeoPixel
+        pixels.show();
+        shortHoldDetected = true;
+      }
+    }
+
+    lastButtonPressTime = millis(); // Update last button press time
   }
-  else if (digitalRead(DOWN_ARROW_GPIO) == HIGH && downArrowPressed) {
-    downArrowPressed = false;
+  else if(digitalRead(BUTTON_GPIO) == LOW){ // Button is not pressed, turn off lights
     pixels.setPixelColor(0, pixels.Color(0, 0, 0)); // Turn off NeoPixel
     pixels.show();
+    longHoldDetected = false;
+    Keyboard.releaseAll(); // Release all keys
+    shortHoldDetected = false;
   }
-
-  // Check for rising edge of Up Arrow input
-  if (digitalRead(UP_ARROW_GPIO) == LOW && !upArrowPressed) {
-    Keyboard.press(KEY_UP_ARROW); // Press Up Arrow key
-    delay(100); // Optional delay for key press
-    Keyboard.release(KEY_UP_ARROW); // Release Up Arrow key
-    pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // Set NeoPixel to Green
-    pixels.show();
-    upArrowPressed = true;
-  }
-  else if (digitalRead(UP_ARROW_GPIO) == HIGH && upArrowPressed) {
-    upArrowPressed = false;
-    pixels.setPixelColor(0, pixels.Color(0, 0, 0)); // Turn off NeoPixel
-    pixels.show();
-  }
-
-  // Wait for a moment before checking inputs again
-  delay(100);
 }
